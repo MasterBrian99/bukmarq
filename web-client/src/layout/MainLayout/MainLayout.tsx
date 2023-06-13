4296272b5479fafc13f7eaf9be266b1c79bebd71
Pasindu Pramodya <pasindudixon25@gmail.com>
2023-06-13 22:09:53 +0530

    keep

--- web-client/src/layout/MainLayout/MainLayout.tsx
+++ web-client/src/layout/MainLayout/MainLayout.tsx
@@ -1,5 +1,38 @@
 import { Outlet } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export default function MainLayout() {
-  return <Outlet />;
+  return (
+    <div className="w-full h-full">
+      <div className="flex relative flex-row">
+        <div className="fixed w-60 h-full bg-cyan-200">
+          <div className="flex justify-between">
+            <div>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+            <div>asd</div>
+          </div>
+        </div>
+        <div className="flex-1 ml-60 w-full h-full bg-green-300">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
--- web-client/src/screens/DashboardScreen/DashboardScreen.tsx
+++ web-client/src/screens/DashboardScreen/DashboardScreen.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const DashboardScreen = () => {
   return (
     <div>
       asd
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
     </div>
   );
 };
